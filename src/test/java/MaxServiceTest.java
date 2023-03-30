e369f4014bb338c7102f68189046dedf89940ee1
LeM <mihail.lebedev88@gmail.com>
2023-03-30 16:49:28 +0600

    100 percent jacoco

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -15,4 +15,17 @@ public class MaxServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldFindMaxIfB() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
